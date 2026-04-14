6be7cb61b2ac4528ee750a534fddd5d161c8af89
lauroware <lauroware@gmail.com>
2026-04-14 15:49:53 -0300

    cambio nombre reset lauro

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,5 +1,8 @@
 import nodemailer from "nodemailer";
 
+console.log("BREVO_SMTP_USER:", process.env.BREVO_SMTP_USER ? "OK" : "FALTA");
+console.log("BREVO_SMTP_KEY:", process.env.BREVO_SMTP_KEY ? "OK" : "FALTA");
+
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
