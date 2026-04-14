c967759d0fad41612bc566fa5157615e6d930f34
lauroware <lauroware@gmail.com>
2026-04-14 15:03:46 -0300

    superadmin

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,10 +1,12 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.GMAIL,
-    pass: process.env.GMAIL_PASSWORD, // app password
+    pass: process.env.GMAIL_PASSWORD,
   },
 });
 
