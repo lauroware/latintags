1ef3a4501c77afbd767c1c3d9948aaa3f163abf6
lauroware <lauroware@gmail.com>
2026-04-14 15:40:42 -0300

    cambio nombre reset lauro

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,23 +1,13 @@
-import * as Brevo from "@getbrevo/brevo";
+import nodemailer from "nodemailer";
 
-const apiInstance = new Brevo.TransactionalEmailsApi();
-apiInstance.authentications["apiKey"].apiKey = process.env.BREVO_API_KEY;
-
-/**
- * Envía un email transaccional via Brevo.
- * Interfaz compatible con el uso anterior de nodemailer:
- *   await sendMail({ to, subject, html })
- */
-const sendMail = async ({ to, subject, html }) => {
-  const email = new Brevo.SendSmtpEmail();
-  email.sender = { email: process.env.EMAIL_FROM, name: "Nexo Tags" };
-  email.to = [{ email: to }];
-  email.subject = subject;
-  email.htmlContent = html;
-  return apiInstance.sendTransacEmail(email);
-};
-
-// Exportamos un objeto con la misma interfaz que nodemailer
-const transporter = { sendMail };
+const transporter = nodemailer.createTransport({
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.BREVO_SMTP_USER,
+    pass: process.env.BREVO_SMTP_KEY,
+  },
+});
 
 export default transporter;
