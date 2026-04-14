15551bbd58877a0eae100d405905ba844a523968
lauroware <lauroware@gmail.com>
2026-04-14 18:22:09 -0300

    claudinary

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -1,6 +1,20 @@
+import { Router } from "express";
 import multer from "multer";
+import upload from "../middlewares/upload.js";
+import { uploadImage } from "../controllers/uploadManager.js";
 
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
+const uploadRouter = Router();
 
-export default upload;
+uploadRouter.post("/", (req, res, next) => {
+  upload.single("image")(req, res, (err) => {
+    if (err instanceof multer.MulterError && err.code === "LIMIT_FILE_SIZE") {
+      return res.status(400).json({ status: "error", message: "La imagen no puede superar 700KB." });
+    }
+    if (err) {
+      return res.status(400).json({ status: "error", message: err.message });
+    }
+    next();
+  });
+}, uploadImage);
+
+export default uploadRouter;
